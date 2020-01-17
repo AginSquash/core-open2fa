434eca9a8de8be91534039b67dfe4b7e575b758b
AginS <agins.main@gmail.com>
2020-01-18 02:18:32 +0300

    Initial Commit

---
+++ core-open2fa/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  core-open2fa
+//
+//  Created by Vlad Vrublevsky on 18.01.2020.
+//  Copyright © 2020 Vlad Vrublevsky. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
