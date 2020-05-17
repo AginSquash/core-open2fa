3c870c49ba869a0cd7e761f6b5f57b9f760460aa
AginS <agins.main@gmail.com>
2020-05-17 23:51:43 +0300

    rename fore package

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "core-open2fa",
+    name: "core_open2fa",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "core-open2fa",
-            targets: ["core-open2fa"]),
+            name: "core_open2fa",
+            targets: ["core_open2fa"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,10 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "core-open2fa",
+            name: "core_open2fa",
             dependencies: ["SwiftOTP", "CryptoSwift"]),
         .testTarget(
-            name: "core-open2faTests",
-            dependencies: ["core-open2fa"]),
+            name: "core_open2faTests",
+            dependencies: ["core_open2fa"]),
     ]
 )
